29fd3c83f90827c60e901563d102aa2589ea92ca
fcabanasm <franco.cabanas@ceinf.cl>
2017-09-24 00:09:58 -0300

    mechanize and twitter

--- Gemfile
+++ Gemfile
@@ -1,6 +1,10 @@
 source 'https://rubygems.org' do
+  ruby '2.4.1'
   gem 'telegram-bot-ruby'
   gem 'httparty'
   gem 'pry'
   gem 'dotenv-rails'
+  gem 'twitter'
+  gem 'json'
+  gem 'mechanize'
 end
--- temblores_chile_bot.rb
+++ temblores_chile_bot.rb
@@ -2,19 +2,46 @@ require 'telegram/bot'
 require 'httparty'
 require 'pry'
 require 'dotenv/load'
+require 'twitter'
+require 'json'
+require 'mechanize'
 
 token = ENV["BOT_API_TOKEN"]
 
 Telegram::Bot::Client.run(token) do |bot|
   bot.listen do |message|
     case message.text
+  
     when '/start'
-      bot.api.send_message(chat_id: message.chat.id, text: "Bot Temblores Chile")
-    when '/test'
-      bot.api.send_message(chat_id: message.chat.id, text: "testing bot")
-
+      bot.api.send_message(chat_id: message.chat.id, text: "Bot Temblores Chile - desarrollado por @fcabanasm")
+      
+    when '/help'
+      bot.api.send_message(chat_id: message.chat.id, text: "Bot Temblores Chile - se encuentra aún en desarrollo")
+  
+    when '/twitter'
+      twitter = Twitter::REST::Client.new do |config|
+        config.consumer_key = ENV["CONSUMER_KEY"]
+        config.consumer_secret = ENV["CONSUMER_SECRET"]
+        config.access_token = ENV["ACCESS_TOKEN"]
+        config.access_token_secret = ENV["ACCESS_TOKEN_SECRET"]
+      end
+      search_term = URI::encode('#temblor ')
+      tweets = twitter.search("#{search_term}", result_type: "recent").take(5)
+      if tweets
+        tweets = tweets.map do |tweet|
+          { name: tweet.user.name, body: tweet.text }
+        end
+        bot.api.send_message(chat_id: message.chat.id, text: "Tweets: #{tweets}")
+      end
+      
+    when '/sismologia'
+      mechanize = Mechanize.new
+      page = mechanize.get('http://www.sismologia.cl/links/ultimos_sismos.html')
+      bot.api.send_message(chat_id: message.chat.id, text: page.at('table').text.strip)
+      
     else
-      bot.api.send_message(chat_id: message.chat.id, text: "Bot Temblores Chile")
+      bot.api.send_message(chat_id: message.chat.id, text: "Bot Temblores Chile (en desarrollo)")
     end
+    
   end
 end
